76590464da14a7d4e87a9dc42de834ecfa847616
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-11 01:46:27 +0530

    Changed

--- index.php
+++ index.php
@@ -143,7 +143,6 @@ if (isset($_GET['download_invoice_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <title>HolidaySeva</title> 
 </head>
