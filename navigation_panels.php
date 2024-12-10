0af25fa454542b5affbcc40bb6e3ef0857cfcd36
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-10 14:07:20 +0530

    Modified

--- navigation_panels.php
+++ navigation_panels.php
@@ -8,8 +8,23 @@
     </button>
 
 </nav> -->
-<style>
-    /* Google Font Import - Poppins */
+<!DOCTYPE html>
+  <!-- Coding by CodingLab | www.codinglabweb.com -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!----===== Boxicons CSS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    
+    <!--<title>Dashboard Sidebar Menu</title>--> 
+    <style>
+        /* Google Font Import - Poppins */
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 *{
     margin: 0;
@@ -318,13 +333,33 @@ body.dark .switch::before{
 body.dark .home .text{
     color: var(--text-color);
 }
-</style>
-<div class="menu-bar">
+    </style>
+</head>
+<body>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <!--<img src="logo.png" alt="">-->
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">Codinglab</span>
+                    <span class="profession">Web developer</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
             <div class="menu">
+
                 <li class="search-box">
                     <i class='bx bx-search icon'></i>
                     <input type="text" placeholder="Search...">
                 </li>
+
                 <ul class="menu-links">
                     <li class="nav-link">
                         <a href="#">
@@ -332,38 +367,45 @@ body.dark .home .text{
                             <span class="text nav-text">Dashboard</span>
                         </a>
                     </li>
+
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-bar-chart-alt-2 icon' ></i>
                             <span class="text nav-text">Revenue</span>
                         </a>
                     </li>
+
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-bell icon'></i>
                             <span class="text nav-text">Notifications</span>
                         </a>
                     </li>
+
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-pie-chart-alt icon' ></i>
                             <span class="text nav-text">Analytics</span>
                         </a>
                     </li>
+
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-heart icon' ></i>
                             <span class="text nav-text">Likes</span>
                         </a>
                     </li>
+
                     <li class="nav-link">
                         <a href="#">
                             <i class='bx bx-wallet icon' ></i>
                             <span class="text nav-text">Wallets</span>
                         </a>
                     </li>
+
                 </ul>
             </div>
+
             <div class="bottom-content">
                 <li class="">
                     <a href="#">
@@ -371,12 +413,14 @@ body.dark .home .text{
                         <span class="text nav-text">Logout</span>
                     </a>
                 </li>
+
                 <li class="mode">
                     <div class="sun-moon">
                         <i class='bx bx-moon icon moon'></i>
                         <i class='bx bx-sun icon sun'></i>
                     </div>
                     <span class="mode-text text">Dark mode</span>
+
                     <div class="toggle-switch">
                         <span class="switch"></span>
                     </div>
@@ -384,7 +428,12 @@ body.dark .home .text{
                 
             </div>
         </div>
+
     </nav>
+
+    <section class="home">
+        <div class="text">Dashboard Sidebar</div>
+    </section>
 
     <script>
         const body = document.querySelector('body'),
@@ -393,12 +442,16 @@ body.dark .home .text{
       searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
+
+
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
+
 searchBtn.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
 })
+
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
     
@@ -410,3 +463,6 @@ modeSwitch.addEventListener("click" , () =>{
     }
 });
     </script>
+
+</body>
+</html>
