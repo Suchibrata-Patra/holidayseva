c5407e287f19759b3ae21447b11849dc70c47dba
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-10 14:02:35 +0530

    Update navigation_panels.php

--- navigation_panels.php
+++ navigation_panels.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding-left:2%;" class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding-left:2%;" class="btn btn-primary" data-bs-toggle="" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
     <a class="navbar-brand" href="#">
         <img src="https://holidayseva.com/wp-content/uploads/2024/06/cropped-holidayseva.com_favicon-1.png" width="30" height="30" class="d-inline-block align-top" alt="">
     </a>
