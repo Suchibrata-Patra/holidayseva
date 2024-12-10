29fd4d05ec5ae37a987a70eb61189f4965f2b49e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-10 13:16:44 +0530

    Update invoice.php

--- invoice.php
+++ invoice.php
@@ -218,13 +218,13 @@ $conn->close();
         <div class="col-md-9">
             <h2 class="text-center mt-5">Existing Invoices</h2>
             <center>
-    <a href="index.php" style="text-decoration: none;">
+                <a href="index.php" style="text-decoration: none; margin-left: 10px;">
+                    <button style="background-color: white; color: black; border: 2px solid black; border-radius: 5px; padding: 12px 30px; font-size: 16px; font-weight: bold; cursor: pointer;  transition: all 0.3s ease;">
+                        Itinery Mode
+                    </button>
+                </a>
+    <a href="invocie.php" style="text-decoration: none;">
         <button style="background-color: black; color: white; border: none; border-radius: 5px; padding: 12px 30px; font-size: 16px; font-weight: bold; cursor: pointer;  transition: all 0.3s ease;">
-            Itinery Mode
-        </button>
-    </a>
-    <a href="invoice.php" style="text-decoration: none; margin-left: 10px;">
-        <button style="background-color: white; color: black; border: 2px solid black; border-radius: 5px; padding: 12px 30px; font-size: 16px; font-weight: bold; cursor: pointer;  transition: all 0.3s ease;">
             Invoice Mode
         </button>
     </a>
