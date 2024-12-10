ddae939d65948f358be1372e0d63e823c90c2af5
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-11 00:15:43 +0530

    Change

--- inquiry.php
+++ inquiry.php
@@ -119,7 +119,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
 
-    <title>Dashboard Sidebar Menu</title>
+    <title>HolidaySeva</title> 
 </head>
 
 <body>
--- market_research.php
+++ market_research.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
 
-    <title>Dashboard Sidebar Menu</title>
+    <title>HolidaySeva</title> 
     <style>
         .grid-container {
   display: grid;
