d70917d1131d4857a607a47de52698d29e10ac2a
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-10 13:00:51 +0530

    Update invoice.php

--- invoice.php
+++ invoice.php
@@ -58,7 +58,6 @@ if (isset($_GET['delete_booking_id'])) {
 $records = $conn->query("SELECT * FROM invoice_data WHERE booking_status NOT IN ('Declined')");
 
 // Close the database connection
-$conn->close();
 
 // Function to generate the invoice as PDF
 function generate_invoice($invoice) {
@@ -109,6 +108,8 @@ if (isset($_GET['download_invoice_id'])) {
     // Generate and download the invoice PDF
     generate_invoice($invoice);
 }
+$conn->close();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
