e1578423c3d7b9730aab99eea0d8aa6cc131628a
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-11 16:17:39 +0530

    Changed cursor

--- invoice_moal_edit_form.php
+++ invoice_moal_edit_form.php
@@ -87,19 +87,19 @@ color: rgb(211, 211, 211);">
 
       <div class="form-group"> 
         <label class="form-label">Customer Name:
-          <input type="text" id="customer_name" name="customer_name" class="form-input" disabled>
+          <input type="text" id="customer_name" name="customer_name" class="form-input" disabled style="cursor:not-allowed";>
         </label>
         <label class="form-label">Mobile No:
-          <input type="text" id="mobile_no" name="mobile_no" class="form-input" disabled>
+          <input type="text" id="mobile_no" name="mobile_no" class="form-input" disabled style="cursor:not-allowed";>
         </label>
       </div>
 
       <div class="form-group">
         <label class="form-label">Pickup Address:
-          <input type="text" id="pickup_address" name="pickup_address" class="form-input" disabled>
+          <input type="text" id="pickup_address" name="pickup_address" class="form-input" disabled style="cursor:not-allowed";>
         </label>
         <label class="form-label">Drop Address:
-          <input type="text" id="drop_address" name="drop_address" class="form-input" disabled style="cursor:not-allowed">
+          <input type="text" id="drop_address" name="drop_address" class="form-input" disabled style="cursor:not-allowed";>
         </label>
       </div>
 
@@ -113,23 +113,23 @@ color: rgb(211, 211, 211);">
       </div>
 
       <label class="form-label">Tour Package:
-        <input type="text" id="tour_package" name="tour_package" class="form-input" >
+        <input type="text" id="tour_package" name="tour_package" class="form-input" disabled style="cursor:not-allowed";>
       </label>
       <div class="form-group">
         <label class="form-label">Date of Journey:
-          <input type="date" id="date_of_journey" name="date_of_journey" class="form-input"  >
+          <input type="date" id="date_of_journey" name="date_of_journey" class="form-input" disabled style="cursor:not-allowed";>
         </label>
         <label class="form-label">No of Adults:
-          <input type="number" id="no_of_adults" name="no_of_adults" class="form-input"  >
+          <input type="number" id="no_of_adults" name="no_of_adults" class="form-input" disabled style="cursor:not-allowed";>
         </label>
       </div>
 
       <div class="form-group">
         <label class="form-label">Cars Provided:
-          <input type="text" id="cars_provided" name="cars_provided" class="form-input"   >
+          <input type="text" id="cars_provided" name="cars_provided" class="form-input" disabled style="cursor:not-allowed";>
         </label>
         <label class="form-label">Number of Cars:
-          <input type="number" id="no_of_cars" name="no_of_cars" class="form-input"  >
+          <input type="number" id="no_of_cars" name="no_of_cars" class="form-input" disabled style="cursor:not-allowed";>
         </label>
       </div>
 
@@ -144,7 +144,7 @@ color: rgb(211, 211, 211);">
           <span class="toggle-slider"></span>
         </label>
         <label class="form-label">Meal Type:
-          <input type="text" id="meal_type" name="meal_type" class="form-input" >
+          <input type="text" id="meal_type" name="meal_type" class="form-input">
         </label>
       </div>
 
