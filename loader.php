4d851715d77706b298cf8096d0a50cd31961de71
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-11 16:02:45 +0530

    Changed Loader

--- loader.php
+++ loader.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Loader with Progress Bar</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <title>Holidayseva</title>
   <style>
     body {
       margin: 0;
