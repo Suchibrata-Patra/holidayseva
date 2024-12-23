16e4f88b0ba18738b4fad47681d0ec305a59fe69
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-23 23:54:06 +0530

    Update invoice_moal_edit_form.php

--- invoice_moal_edit_form.php
+++ invoice_moal_edit_form.php
@@ -103,9 +103,7 @@ scrol
         <label class="form-label">Mobile No:
           <input type="text" id="mobile_no" name="mobile_no" class="form-input"  style="cursor:pointer"; >
         </label>
-        <label class="form-label" for="mobile_no">Mobile No:</label>
-<input type="text" id="mobile_no" name="mobile_no" class="form-input" style="cursor: pointer;">
-
+        
       </div>
 
       <div class="form-group">
