e39a3aa02d57d2173f258b65f0ecef631cf6735d
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-30 12:20:45 +0530

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -97,7 +97,7 @@ function generate_invoice($invoice) {
     $tour_details_html = '<div class="important">
         <div class="text">
             <strong>
-                <div style="display: flex; align-items: center; gap: 10px;">
+                <div style="display: flex; align-items: center; gap:1px;">
                     <div style="display: flex; align-items: center; justify-content: center; background-color: red; color: white; border-radius: 50%; width: 30px; height: 20px;">
                         <div style="text-align: center;">
                             <span style="font-size: 12px; display: block;">Day</span>
