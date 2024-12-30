97d9a8a9d08834c91cd5181095e8078cbc706e81
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-30 13:04:07 +0530

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -96,17 +96,17 @@ function generate_invoice($invoice) {
     if ($invoice['tour_package'] == '1N/2D Complete GangaSagar Yatra Package') {
     $tour_details_html = '<div class="important">
         <div class="text">
-            <strong>
-                <div style="display: flex; align-items: center; gap:1px;">
-                    <div style="align-items: center; justify-content: center; background-color: red; color: white; border-radius: 50%; width: 50px; height: 50px;">
-                        <div style="text-align: center;">
-                            <span style="font-size: 12px; display: block;">Day</span>
-                            <span style="font-size: 20px; font-weight: bold;">01</span>
-                        </div>
+            <div style="display: flex; align-items: center; gap:1px;">
+                <div style="align-items: center; justify-content: center; background-color: red; color: white; border-radius: 50%; width: 50px; height: 50px;">
+                    <div style="text-align: center;">
+                        <span style="font-size: 12px; display: block;">Day</span>
+                        <span style="font-size: 20px; font-weight: bold;">01</span>
                     </div>
                 </div>
-                <span>Kolkata to Ganga Sagar Yatra Package (130 Km./5:00 Hrs):</span> 
-            </strong>
+                <strong>
+                    <span>Kolkata to Ganga Sagar Yatra Package (130 Km./5:00 Hrs):</span> 
+                </strong>
+                </div>
 
             <br>
             <strong>Meet & Greet</strong> -
