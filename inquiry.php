b5ca147438416a139d2d6fd3b884444b86bfa405
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-30 12:20:36 +0530

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -98,7 +98,7 @@ function generate_invoice($invoice) {
         <div class="text">
             <strong>
                 <div style="display: flex; align-items: center; gap: 10px;">
-                    <div style="display: flex; align-items: center; justify-content: center; background-color: red; color: white; border-radius: 50%; width: 80px; height: 80px; font-family: Arial, sans-serif;">
+                    <div style="display: flex; align-items: center; justify-content: center; background-color: red; color: white; border-radius: 50%; width: 30px; height: 20px;">
                         <div style="text-align: center;">
                             <span style="font-size: 12px; display: block;">Day</span>
                             <span style="font-size: 20px; font-weight: bold;">01</span>
