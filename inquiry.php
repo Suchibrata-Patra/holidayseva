75b5ec1605ffff9ed1f5e35362df427872e5b67b
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-30 16:15:53 +0530

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -98,7 +98,7 @@ function generate_invoice($invoice) {
         <div class="text">
             
             <div style="display: flex; align-items: left; gap: 10px;">
-                <span style="background-color:red; color:white; border-radius: 50%;text-align: left;padding:7px 6px 7px 11px;">1
+                <span style="background-color:red; color:white; border-radius: 50%;text-align: left;padding:7px 6px 7px 10.5px;">1
                 </span>
                     <strong>Kolkata to Ganga Sagar Yatra</strong>
             </div>
@@ -118,7 +118,7 @@ function generate_invoice($invoice) {
             <br><br>
 
             <div style="display: flex; align-items: left; gap: 10px;">
-                <span style="background-color:red; color:white; border-radius: 50%;text-align: left;padding:7px 7px 7px 12px;">2
+                <span style="background-color:red; color:white; border-radius: 50%;text-align: left;padding:7px 6px 7px 10.5px;">2
                 </span>
                 <strong> Enroute to Kolkata</strong>
             </div>
