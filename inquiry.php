7a0cb358dd362bbd798feede3cf023edfbf13c3f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-30 11:06:43 +0530

    Changed

--- inquiry.php
+++ inquiry.php
@@ -96,7 +96,11 @@ function generate_invoice($invoice) {
     if ($invoice['tour_package'] == '1N/2D Complete GangaSagar Yatra Package') {
     $tour_details_html = '<div class="important">
         <div class="text">
-            <strong> Day 1 - Kolkata to Ganga Sagar Yatra Package (130 Km./5:00 Hrs):</strong><br>
+            <strong>
+            <div style="display:flex;">
+            <button style="background-color: red;color: white;border-radius: 50px;border:none;padding: 10px;">Day 01</button> Kolkata to Ganga Sagar Yatra Package (130 Km./5:00 Hrs):</strong>
+            </div>
+            <br>
             <strong>Meet & Greet</strong> -
             Upon arrival at the Airport/Station/Hotel/City, you will be warmly welcomed and driven to Gangasagar, an island
             at the confluence of the Ganga River and the Bay of Bengal, renowned for the spiritually significant Kapil Muni
